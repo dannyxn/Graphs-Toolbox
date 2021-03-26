b78d2c38037bf89ecbcead189501f743bbf67360
Dariusz Lesiecki <65817574+dariuszlesiecki@users.noreply.github.com>
2021-03-26 14:47:25 +0100

    updated graph_generators and added task 1.3

--- random_generation/graph_generators.py
+++ random_generation/graph_generators.py
@@ -4,50 +4,37 @@ from random import randint, random
 from algorithms.coherent_component import GraphRepresentationType, GraphRepresentation, CoherentComponentFinder
 
 
-def G_n_l(n, l) -> GraphRepresentation:
-    G = defaultdict(list)
-    [G.setdefault(x, []) for x in range(n)]
-    for _ in range(l):
-        x = randint(0, n - 1)
-        y = randint(0, n - 1)
+def generate_with_edges(number_of_nodes: int, number_of_edges: int) -> GraphRepresentation:
+    G = defaultdict(list, {degree: [] for degree in range(number_of_nodes)})
+    for _ in range(number_of_edges):
+        x = randint(0, number_of_nodes - 1)
+        y = randint(0, number_of_nodes - 1)
         while True:
             if y != x and y not in G[x]:
                 break
-            y = randint(0, n - 1)
+            y = randint(0, number_of_nodes - 1)
         G[x].append(y)
         G[y].append(x)
-    G = sorted(G.items())
-    G = defaultdict(list, G)
-    BG = defaultdict(list)
-    [BG.setdefault(x, []) for x in range(n)]
-    for i in G:
-        l = []
-        for j in G[i]:
-            l.append(j)
-        l.sort()
-        for j in l:
-            BG[i].append(j)
-    graph = GraphRepresentation(GraphRepresentationType.ADJACENCY_LIST, BG)
-    return graph
+    return GraphRepresentation(GraphRepresentationType.ADJACENCY_LIST, G)
+
+
+def generate_with_probability(number_of_nodes: int, probability: float):
+    G = defaultdict(list, {degree: [] for degree in range(number_of_nodes)})
+    for node1 in range(number_of_nodes):
+        for node2 in range(node1, number_of_nodes):
+            if random() < probability:
+                if node1 != node2:
+                    G[node1].append(node2)
+                    G[node2].append(node1)
+    print(G)
+    return GraphRepresentation(GraphRepresentationType.ADJACENCY_LIST, G)
 
 
 def generate_connected_graph(nodes, paths) -> GraphRepresentation:
     finder = CoherentComponentFinder()
     while True:
-        graph = G_n_l(nodes, paths)
+        graph = generate_with_edges(nodes, paths)
         graph.convert(GraphRepresentationType.ADJACENCY_MATRIX)
         if finder.check_if_graph_is_connected(graph):
             break
     return graph
-
-
-# ta ponizej dalej zje*ana :)
-def G_n_p(n, p):
-    G = [[] for _ in range(n)]
-    for i in range(n):
-        for j in range(i, n):
-            if random() < p:
-                if i != j:
-                    G[i].append(j + 1)
-                    G[j].append(i + 1)
-    return G
