1aa281f867052134167f75882191a1da3346c4e3
Dariusz Lesiecki <65817574+dariuszlesiecki@users.noreply.github.com>
2021-04-07 15:26:01 +0200

    fix

--- tests/Task_4_1.py
+++ tests/Task_4_1.py
@@ -5,3 +5,15 @@ import networkx as nx
 if __name__ == "__main__":
     G = generate_diGraph_with_probability(7,0.3)
     display_nx_DiGraph(G)
+
+    g = nx.to_dict_of_lists(G)
+    print("\n adjency list")
+    print(g)
+
+    g = nx.to_numpy_matrix(G)
+    print("\n adjency matrix")
+    print(g)
+
+    g = -nx.incidence_matrix(G, oriented=True)
+    print("\n incidence matrix")
+    print(g.toarray())
