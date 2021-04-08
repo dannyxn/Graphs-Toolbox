f1720ebe9f9f7f9bfff4b9b7d926ee2867e5f02d
DESKTOP-SOT8G9N\dawid <dawid.chara.9@gmail.com>
2021-04-09 01:27:55 +0200

    Task_2_4 - Finding euler cycle in graph - cycle displayed as string sequence of numbers and arrows

--- tests/Task_2_4.py
+++ tests/Task_2_4.py
@@ -2,10 +2,16 @@ import networkx as nx
 from core.graph_representation import GraphRepresentationType
 from random_generation.graph_generators import generate_random_euler_graph
 from visualization.nx_graph import display_nx_graph
+from algorithms.euler_cycle import EulerCycleFinder
 
 
 if __name__ == "__main__":
     graph = generate_random_euler_graph(10)
     graph.convert(GraphRepresentationType.ADJACENCY_LIST)
+
+    finder = EulerCycleFinder()
+    finder.find(graph)
+
     graph = nx.Graph(graph.math_repr)
     display_nx_graph(graph)
+
