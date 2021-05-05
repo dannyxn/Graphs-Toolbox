dc02ac0d12cd2820f8d57fb000506790c6efb614
Dariusz Lesiecki <65817574+dariuszlesiecki@users.noreply.github.com>
2021-05-05 18:36:05 +0200

    fix

--- tests/Task_2_5.py
+++ tests/Task_2_5.py
@@ -2,9 +2,11 @@ from algorithms.graph_randomization import randomize_graph
 from core.graph_representation import GraphRepresentationType
 from random_generation.graph_generators import k_regular_graph
 
-
 if __name__ == "__main__":
-    graph = k_regular_graph(7, 2)
+    graph = k_regular_graph(6, 3)
+
+    #graph.display()
+
     randomized_graph = randomize_graph(graph, 100)
     randomized_graph.convert(GraphRepresentationType.ADJACENCY_LIST)
     randomized_graph.display()
