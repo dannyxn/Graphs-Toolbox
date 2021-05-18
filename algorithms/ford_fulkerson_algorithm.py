4cdb512ffb8749b528c3bb62ad9d32d185856fc7
DESKTOP-SOT8G9N\dawid <dawid.chara.9@gmail.com>
2021-05-18 21:49:51 +0200

    Doctsting add

--- algorithms/ford_fulkerson_algorithm.py
+++ algorithms/ford_fulkerson_algorithm.py
@@ -2,7 +2,7 @@ import networkx as nx
 
 
 class FordFulkersonAlgorithm:
-    def __init__(self, graph):
+    def __init__(self, graph: nx.DiGraph):
         self.graph = nx.to_numpy_array(graph)
         self.number_of_nodes = len(graph)
 
@@ -21,10 +21,13 @@ class FordFulkersonAlgorithm:
 
         return True if visited[t] else False
 
-    def find_max_flow(self):
+    def find_max_flow(self) -> int:
+        """
+        Find maximum flow between first node and last
+        :return int: Maximum flow
+        """
         parent = [-1] * self.number_of_nodes
-        max_flow = 0
-        source, sink = 0, self.number_of_nodes - 1
+        source, sink, max_flow = 0, self.number_of_nodes - 1, 0
 
         while self.BFS(source, sink, parent):
             path_flow = float("Inf")
