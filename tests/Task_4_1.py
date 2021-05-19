1fc18b328f2aa9bb56aee4a7da577ee2f0014018
Dariusz Lesiecki <65817574+dariuszlesiecki@users.noreply.github.com>
2021-05-19 18:26:51 +0200

    4.1 printing fix

--- tests/Task_4_1.py
+++ tests/Task_4_1.py
@@ -1,19 +1,29 @@
+import networkx as nx
+import numpy as np
+
 from random_generation.graph_generators import generate_digraph_with_probability
 from visualization.nx_graph import display_nx_digraph
-import networkx as nx
 
 if __name__ == "__main__":
-    G = generate_digraph_with_probability(7, 0.3)
+    G = generate_digraph_with_probability(10, 0.15)
     display_nx_digraph(G)
 
     g = nx.to_dict_of_lists(G)
-    print("\n adjency list")
-    print(g)
+    print("\n adjacency list")
+    for node, neighbours in g.items():
+        tmp = [str(x) for x in neighbours]
+        print(f"{node}: {' '.join(tmp)}")
 
-    g = nx.to_numpy_matrix(G)
-    print("\n adjency matrix")
-    print(g)
+    g = nx.to_numpy_array(G)
+    print("\n adjacency matrix")
+    for row in g:
+        tmp = [str(int(x)) for x in row]
+        print(f"{' '.join(tmp)}")
 
     g = -nx.incidence_matrix(G, oriented=True)
+    g = g.toarray()
     print("\n incidence matrix")
-    print(g.toarray())
+    for row in g:
+        tmp = '\t'.join([str(int(x)) for x in row])
+        print(f"{tmp}")
+    print()
